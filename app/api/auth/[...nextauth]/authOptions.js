f2c755a4dd84c89ecc09ab817f9670d5e2f0b030
latioms <73307766+latioms@users.noreply.github.com>
2024-10-01 05:57:34 +0100

    feat: Ajouter les options d'authentification pour NextAuth
    Ajout du fichier authOptions.js dans le répertoire app/api/auth/[...nextauth] pour configurer les options d'authentification de NextAuth. Les options incluent la configuration des fournisseurs d'authentification, l'autorisation des utilisateurs, les callbacks JWT et session, ainsi que la page de connexion personnalisée.
    
    Les dépendances next-auth, react-hot-toast et react-spinners ont également été ajoutées.
    
    Les fichiers tsconfig.json, env.local.old, route.js et providers.jsx ont été modifiés pour inclure les nouveaux fichiers et répertoires.

---
+++ app/api/auth/[...nextauth]/authOptions.js
@@ -0,0 +1,60 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import prisma from '../../../../lib/prisma'
+import bcrypt from 'bcryptjs'
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null
+        }
+
+        const user = await prisma.utilisateur.findUnique({
+          where: { email: credentials.email }
+        })
+
+        if (!user) {
+          return null
+        }
+
+        const isPasswordValid = await bcrypt.compare(credentials.password, user.password)
+
+        if (!isPasswordValid) {
+          return null
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          nom_user: user.nom_user,
+          prenom_user: user.prenom_user,
+          role: user.role,
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.user.role = token.role
+      return session
+    }
+  },
+  pages: {
+    signIn: '/login',
+  }
+}
+
